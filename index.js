7cd696b5041b7891ee587a174f71417d5d439a8a
Joeway <joewaybanh@hotmail.com>
2020-09-30 15:59:48 +0200

    updated colours to names

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require("chalk")
+
+console.log(chalk.bgBlue("Daute"))
+console.log(chalk.redBright("Kaspars"))
+console.log(chalk.bgCyanBright("Martin"))
+console.log(chalk.bgYellowBright("Matt"))
+console.log(chalk.whiteBright("Rado"))
